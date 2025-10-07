9af46be48aa7637ca61c697899dc39ec862d961b
Thucdzio <letienthuc2004@gmail.com>
2025-10-07 22:33:02 +0700

    test git action

--- main.cpp
+++ main.cpp
@@ -33,52 +33,19 @@ BmiResult bmi_calculate(double weight_kg, double height_m)
 }
 int main()
 {
-    struct TestCase
-    {
-        int id;
-        double w;
-        double h;
-    };
-
-    vector<TestCase> tests = {
-        {0, -1, 1.7},
-        {1, 0, 1.70},
-        {2, 53.4, 1.70},
-        {3, 53.5, 1.70},
-        {4, 71.9, 1.70},
-        {5, 72.3, 1.70},
-        {6, 86.3, 1.70},
-        {7, 86.7, 1.70},
-        {8, 100.9, 1.70},
-        {9, 101.2, 1.70},
-        {10, 115.3, 1.70},
-        {11, 115.6, 1.70},
-        {12, 115.9, 1.70}};
-
-    cout << left << setw(4) << "ID"
-         << setw(8) << "w"
-         << setw(8) << "h"
-         << setw(10) << "BMI"
-         << "Category\n";
-
-    for (auto &tc : tests)
-    {
-        cout << setw(4) << tc.id
-             << setw(8) << tc.w
-             << setw(8) << tc.h;
-
-        try
-        {
-            BmiResult res = bmi_calculate(tc.w, tc.h);
-            cout << setw(10) << fixed << setprecision(2) << res.bmi_display
-                 << res.category;
-        }
-        catch (const invalid_argument &e)
-        {
-            cout << setw(10) << "-" << e.what();
-        }
-        cout << "\n";
-    }
+    assert(bmi_calculate(-1, 1.7).category == "Underweight");
+    assert(bmi_calculate(0, 1.70).category == "Normal");
+    assert(bmi_calculate(53.4, 1.70).category == "Normal");
+    assert(bmi_calculate(53.5, 1.70).category == "Overweight");
+    assert(bmi_calculate(71.9, 1.70).category == "Overweight");
+    assert(bmi_calculate(72.3, 1.70).category == "Obesity I");
+    assert(bmi_calculate(86.3, 1.70).category == "Obesity I");
+    assert(bmi_calculate(86.7, 1.70).category == "Obesity II");
+    assert(bmi_calculate(100.9, 1.70).category == "Obesity II");
+    assert(bmi_calculate(101.2, 1.70).category == "Obesity III");
+    assert(bmi_calculate(115.3, 1.70).category == "Obesity III");
+    assert(bmi_calculate(115.6, 1.70).category == "Obesity III");
+    assert(bmi_calculate(115.9, 1.70).category == "Obesity III");
 
     return 0;
 }
